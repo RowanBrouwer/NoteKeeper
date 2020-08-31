79353ec58caf4aee4ddb52e78331446e029ac79e
rowan brouwer <brouwerrowan@gmail.com>
2020-08-31 14:45:53 +0200

    Add project files.

---
+++ NoteKeeper/NoteKeeper/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using NoteKeeper.ViewModels;
+
+namespace NoteKeeper.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
